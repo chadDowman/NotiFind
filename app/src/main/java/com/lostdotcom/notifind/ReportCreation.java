3a3214baa70614c5f0bc3830b93c3e5acd16d4f2
chadDowman <Chadwddk@gmail.com>
2020-08-01 19:05:34 +0200

    Update 6

--- app/src/main/java/com/lostdotcom/notifind/ReportCreation.java
+++ app/src/main/java/com/lostdotcom/notifind/ReportCreation.java
@@ -3,12 +3,33 @@ package com.lostdotcom.notifind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ReportCreation extends AppCompatActivity {
+
+    EditText txtName;
+    EditText txtSurname;
+    EditText txtAge;
+    EditText txtEyeColor;
+    EditText txtWeight;
+    EditText txtHeight;
+    EditText txtLastSeenLocation;
+    EditText txtDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_creation);
+
+        txtName = (EditText) findViewById(R.id.name);
+        txtSurname = (EditText) findViewById(R.id.surname);
+        txtAge = (EditText) findViewById(R.id.age);
+        txtEyeColor = (EditText) findViewById(R.id.eyecolor);
+        txtWeight = (EditText) findViewById(R.id.weight);
+        txtHeight = (EditText) findViewById(R.id.height);
+        txtLastSeenLocation = (EditText) findViewById(R.id.lastLocation);
+        txtDescription = (EditText) findViewById(R.id.description);
     }
+
+
 }
