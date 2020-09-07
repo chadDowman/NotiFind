2214aee2d02a6edd3b8e0da62b645e476bfdc7dd
LidraFoxfire <vanderwath.ulrich121@gmail.com>
2020-09-07 03:48:12 +0200

    upload

---
+++ app/src/main/java/com/lostdotcom/notifind/admin_list_items.java
@@ -0,0 +1,14 @@
+package com.lostdotcom.notifind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class admin_list_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_list_items);
+    }
+}
